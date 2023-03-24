fbda3f41f57f0eab93df69926680a8f7e2bc4ae8
Yousef Wael <yousef.wael2002@gmail.com>
2023-03-24 02:55:10 +0200

    added comments

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ with st.sidebar:
     frequency = st.number_input("Frequency") # Getting the sine wave frequency
     amplitude = st.number_input("Amplitude") # Getting the sine wave amplitude
     
-
+# Siganl Sampling
 rate, data = wav.read(uploaded_file)
 sampling_freq= rate
 nyquist_freq = sampling_freq / 2
@@ -25,6 +25,7 @@ sampling_rate = nyquist_freq
 samples = np.arange(0, len(data), sampling_rate)
 sampled_data = data[np.round(samples).astype(int)]
 
+# Plotting sampled signal
 fig1, ax1 = plt.subplots()
 ax1.stem(sampled_data)
 ax1.set_xlabel('Time', fontsize=15)
@@ -32,12 +33,13 @@ ax1.set_ylabel('Amplitude', fontsize=15)
 ax1.set_title('Sampled Signal', fontsize=20)
 st.pyplot(fig1)
 
+# Reconstructing Signal
 new_len = len(data)
 resampled_data = signal.resample(sampled_data, new_len)
-
 interpolator = interpolate.interp1d(np.arange(0, new_len), resampled_data, kind='linear')
 reconstructed_data = interpolator(np.linspace(0, new_len -1, new_len))
 
+# Plotting reconstructed signal
 fig2, ax2 = plt.subplots()
 ax2.plot(reconstructed_data)
 ax2.set_xlabel('Time', fontsize=15)
