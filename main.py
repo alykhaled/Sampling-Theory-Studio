30c51ed69296af730a21b102bc9fdc457ce7a016
Aly Khaled <alykhaled2001@live.com>
2023-05-18 12:26:50 +0300

    Fix

--- main.py
+++ main.py
@@ -60,7 +60,13 @@ def sidebar():
     maxFrequency = st.session_state['maxFrequency']
     # st.write(maxFrequency)
     # st.sidebar.write('Max Frequency: {}'.format(st.session_state.maxFrequency))
-    frequency = st.sidebar.slider('Frequency from 0 to 4 FMAX', 1, int(4*maxFrequency),1, key='frequency', on_change=updateSignal)
+    # frequencyType = st.sidebar.radio('Frequency Type', ['Hz', 'FMAX'], key='frequencyType')
+    # if frequencyType == 'Hz':
+    #     frequency = st.sidebar.slider('Frequency', 1, 100, 1, key='frequency', on_change=updateSignal)
+    # else:
+    #     frequency = st.sidebar.slider('Frequency', 1, 4*int(maxFrequency), 1, key='frequency', on_change=updateSignal)
+        # frequency = frequency / maxFrequency
+    frequency = st.sidebar.slider('Frequency', 1, 4*int(maxFrequency), 1, key='frequency', on_change=updateSignal)
 
     # Noise Settings Controls
     st.sidebar.header('Noise Settings')
@@ -103,7 +109,7 @@ def plotSignal():
 
 def main():
     st.set_page_config(page_title='Signal Sampling Studio', page_icon=':heart:', layout='wide', initial_sidebar_state='auto')
-    st.title('Signal Sampling Studio :heart:')
+    st.title('Signal Sampling Studio')
     sidebar()
     plotSignal()
 
