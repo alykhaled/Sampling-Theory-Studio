067af6d52d75bad45ad0d620e2a30951aa8f94f7
Yousef Wael <yousef.wael2002@gmail.com>
2023-05-23 23:31:25 +0300

    sinc interpolation

--- src/ReconstructedSignal.py
+++ src/ReconstructedSignal.py
@@ -5,6 +5,7 @@ from .Signal import Signal
 from .SampledSignal import SampledSignal
 import scipy.interpolate as interpolate
 import scipy.signal as signal
+from scipy.special import sinc
 
 class ReconstructedSignal(SampledSignal):
     def __init__(self, data, time, frequency, addNoise, noise, scroll=0, zoom=1):
@@ -12,22 +13,26 @@ class ReconstructedSignal(SampledSignal):
         self.reconstructedTime = []
         self.reconstructedSignal = []
 
+    
     def reconstruct(self):
-        """
-        Reconstruct the signal from the sampled signal using Nyquist-Shannon Sampling Theorem
-        This function should be called after sample()
-        Then update the reconstructedTime and reconstructedSignal variables
-        return: reconstructedTime, reconstructedSignal
-        """
-        super().sample() # Sample the signal
+        super().sample()  # Sample the signal
         if len(self.time) == 0:
             return
+        
         # TODO: Reconstruct the signal
         new_len = len(self.data)
         resampled_data = signal.resample(self.samplingPointsSignal, new_len)
-        interpolator = interpolate.interp1d(np.arange(0, new_len), resampled_data, kind='linear')
-        self.reconstructedSignal = interpolator(np.linspace(0, new_len -1, new_len))
+        reconstructed_time = np.linspace(0, new_len - 1, new_len)
+        
+        # Apply sinc interpolation
+        reconstructed_signal = np.zeros_like(reconstructed_time)
+        for i, t in enumerate(reconstructed_time):
+            reconstructed_signal[i] = np.sum(resampled_data * np.sinc(t - np.arange(len(resampled_data))))
+        
+        self.reconstructedSignal = reconstructed_signal
         self.reconstructedTime = self.time
+        
+        return self.reconstructedTime, self.reconstructedSignal
 
     def plotSampled(self):
         # Draw Graph
